991dc01565bef34ffc41812ede47ceeb8569d93e
Leonardo Uieda <leouieda@gmail.com>
2020-04-12 19:55:16 +0100

    Add a basic recipe renderer
    Using it to experiment the recipe definition. Does the basic math
    already but does not support titles, instructions, author, etc.

--- padeiro/core.py
+++ padeiro/core.py
@@ -1,10 +1,45 @@
 """
 Defines the recipe rendering functionality
 """
+import math
 
 
 def render(recipe, flour_weight):
     """
     Replace percentages in a recipe with weights.
     """
-    pass
+    check_recipe(recipe)
+    rendered = dict()
+    if "levain" in recipe:
+        levain = recipe["levain"]["recipe"]
+        levain_weight = flour_weight * recipe["levain"]["percentage"] / 100
+        rendered["levain"] = render(
+            levain,
+            flour_weight=levain_flour_weight(levain, total_weight=levain_weight),
+        )
+    ingredients = [name for name in recipe if name != "levain"]
+    for ingredient in ingredients:
+        rendered[ingredient] = dict()
+        for name, percentage in recipe[ingredient].items():
+            rendered[ingredient][name] = math.floor(flour_weight * percentage / 100)
+            if "levain" in recipe and ingredient in rendered["levain"]:
+                if name in rendered["levain"][ingredient]:
+                    rendered[ingredient][name] -= rendered["levain"][ingredient][name]
+    return rendered
+
+
+def levain_flour_weight(recipe, total_weight):
+    """
+    Figure out the weight of flour in the levain from the total weight.
+    """
+    total_liquid = sum(recipe["liquid"][i] for i in recipe["liquid"])
+    flour = total_weight / (1 + total_liquid / 100)
+    return flour
+
+
+def check_recipe(recipe):
+    """
+    Assert that a recipe conforms to the right style.
+    """
+    if "levain" in recipe:
+        check_recipe(recipe["levain"]["recipe"])
--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -5,5 +5,22 @@ from padeiro import render
 
 
 def test_render():
-    "Placeholder test"
-    render(recipe={"title": "Basic Rye"}, flour_weight=1000)
+    "Render a basic recipe "
+    recipe = {
+        "flour": {"white": 70, "whole wheat": 30},
+        "liquid": {"water": 63},
+        "levain": {
+            "percentage": 20,
+            "recipe": {"flour": {"whole wheat": 100}, "liquid": {"water": 100}},
+        },
+        "extra": {"salt": 2},
+    }
+    result = render(recipe, flour_weight=1000)
+    # 100g of whole wheat and 100g of water is in the levain
+    expected = {
+        "flour": {"white": 700, "whole wheat": 200},
+        "liquid": {"water": 530},
+        "levain": {"flour": {"whole wheat": 100}, "liquid": {"water": 100}},
+        "extra": {"salt": 20},
+    }
+    assert result == expected
